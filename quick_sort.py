0ac9bfb033b6316aa4788036ed7348411c008a85
rubyking007 <134137656+rubyking007@users.noreply.github.com>
2023-05-21 09:33:41 +0800

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,1 +1,10 @@
-
+# 使用python 实现快排算法
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[0]
+    less = [x for x in arr[1:] if x <= pivot]
+    greater = [x for x in arr[1:] if x > pivot]
+    
+    return quick_sort(less) + [pivot] + quick_sort(greater)
