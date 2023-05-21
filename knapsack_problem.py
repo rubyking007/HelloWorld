b8542c29c55576bd09e01bad838f6aa2b72b6a9c
rubyking007 <134137656+rubyking007@users.noreply.github.com>
2023-05-21 09:32:55 +0800

    Update knapsack_problem.py

--- knapsack_problem.py
+++ knapsack_problem.py
@@ -1,1 +1,13 @@
+# 使用python实现 动态规划算法 - 背包问题
+def knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
+    for i in range(1, n + 1):
+        for j in range(1, capacity + 1):
+            if weights[i - 1] > j:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]])
+
+    return dp[n][capacity]
